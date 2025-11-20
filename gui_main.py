cfb1d356bf8420357ef3d3e744da249b91e61eec
PensiveEagle <halljf96@gmail.com>
2025-11-20 22:43:08 +0000

    Added Add buton functionality
    Wired Add button up to add the typed task to the task data

--- gui_main.py
+++ gui_main.py
@@ -1,10 +1,33 @@
 import functions as func
 import FreeSimpleGUI as fsg
 
-label = fsg.Text( 'Type in a task' )
-input_box = fsg.InputText( tooltip = 'Enter a task', default_text = 'New task' )
-add_button = fsg.Button( 'Add' )
+loop = True
 
-window = fsg.Window( 'Task List App', layout = [[label], [input_box, add_button]] )
-window.read()
+func.init_data()
+
+label = fsg.Text( 'Type in a task' )
+input_box = fsg.InputText( tooltip = 'Enter a task', key = 'task' )
+add_button = fsg.Button( 'Add' )
+edit_button = fsg.Button( 'Edit' )
+
+window = fsg.Window( 'Task List App', 
+                    layout = [[label, input_box, add_button]],
+                    font = ( 'Helvetica', 12 ) )
+
+
+while loop:
+    event, values = window.read() #type: ignore
+    print( event )
+    print( values )
+    
+    match event:
+        
+        case 'Add':
+            task_list = func.get_tasks()
+            new_task = values['task']
+            task_list.append( new_task + '\n' )
+            func.write_tasks( task_list )
+            
+
+
 window.close()
