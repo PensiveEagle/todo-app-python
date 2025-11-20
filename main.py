5e2df5623ad7484daf832c1ba61817ebc9276efe
PensiveEagle <halljf96@gmail.com>
2025-11-20 16:12:21 +0000

    Added functions for interacting with data

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 prompts = {'task': 'Enter a task: ', 'action': 'Type add, show, edit, complete, or exit: ', 'grab': 'Which item would you like to edit/complete?: ', 'edit': 'Enter new task: '}
 
+def get_tasks( filepath: str = 'task_data.txt' ) -> list:
+    ''' Read a text file and return the list of tasks '''
+    with open( filepath, 'r' ) as task_file:
+        task_list = task_file.readlines()
+    return task_list
+
+def write_tasks( tasks_arg: list , filepath: str = 'task_data.txt' ) -> none:
+    ''' Writes a list of tasks out to a text file '''
+    with open( filepath, 'w' ) as task_file:
+        task_file.writelines( tasks_arg )
+    
 loop = True
 
 with open( 'task_data.txt', 'a' ) as task_file:
@@ -11,54 +22,47 @@ while loop:
     if 'add' in action:
         new_task = action[4:]
             
-        with open( 'task_data.txt', 'r' ) as task_file:
-            task_list = task_file.readlines()
+        task_list = get_tasks()
         
-        task_list.append( new_task )
+        task_list.append( new_task + '\n' )
         
-        with open( 'task_data.txt', 'w' ) as task_file:
-            task_file.writelines( task_list )
-                
-            print( f'\n{new_task} added to list!\n' )
+        write_tasks( task_list )
     
     elif 'show' in action:
-        with open( 'task_data.txt', 'r' ) as task_file:
-            task_list = task_file.readlines()
+        task_list = get_tasks()
         
-        print()
         for index, item in enumerate(task_list):
             print( f'{index + 1}:- {item}' )
-        print()
     
     elif 'edit' in action:
-        with open( 'task_data.txt', 'r' ) as task_file:
-            task_list = task_file.readlines()
-        
-        grab_task = int( action[5:] ) - 1
-        old_task = task_list[grab_task]
-        print( f'\nCurrent task: {old_task}\n' )
-        
-        edit_task = input( prompts['edit'] )
-        print()
-        task_list[grab_task] = edit_task
-        
-        with open( 'task_data.txt', 'w' ) as task_file:
-            task_file.writelines( task_list )
+        try:
+            task_list = get_tasks()
             
-        print( f'\n{old_task} replaced with {edit_task}\n' )
+            grab_task = int( action[5:] ) - 1
+            old_task = task_list[grab_task]
+            
+            edit_task = input( prompts['edit'] )
+            task_list[grab_task] = edit_task + '\n'
+            
+            write_tasks( task_list )
+        
+        except ValueError:
+            print( "Your command was invalid!" )
+            continue
     
     elif 'complete' in action:
-        with open( 'task_data.txt', 'r' ) as task_file:
-            task_list = task_file.readlines()
-        
-        grab_task = int( action[5:] ) - 1
-        complete_task = task_list[grab_task]
-        task_list.pop( grab_task )
-        
-        with open( 'task_data.txt', 'w' ) as task_file:
-            task_file.writelines( task_list )
-        
-        print( f'\n{complete_task} marked as complete!\n' )
+        try:
+            task_list = get_tasks()
+            
+            grab_task = int( action[9:] ) - 1
+            complete_task = task_list[grab_task]
+            task_list.pop( grab_task )
+            
+            write_tasks( task_list )
+            
+        except ValueError:
+            print( "Your command was invalid!" )
+            continue
 
     elif 'exit':
         break
