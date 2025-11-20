c256f4a6d4f694615cf0cf140d66618d85ae7dbf
PensiveEagle <halljf96@gmail.com>
2025-11-20 20:09:11 +0000

    Created init_data function
    Created function in functions.py
    To initialise the data file for storing task data

--- functions.py
+++ functions.py
@@ -1,10 +1,17 @@
-def get_tasks( filepath: str = 'task_data.txt' ) -> list:
+FILEPATH = 'task_data.txt'
+
+def init_data( filepath: str = FILEPATH ):
+    '''Initialises the data text file'''
+    with open( filepath, 'a' ) as task_file:
+        pass
+
+def get_tasks( filepath: str = FILEPATH ) -> list:
     ''' Read a text file and return the list of tasks '''
     with open( filepath, 'r' ) as task_file:
         task_list = task_file.readlines()
     return task_list
 
-def write_tasks( tasks_arg: list , filepath: str = 'task_data.txt' ) -> None:
+def write_tasks( tasks_arg: list , filepath: str = FILEPATH ) -> None:
     ''' Writes a list of tasks out to a text file '''
     with open( filepath, 'w' ) as task_file:
         task_file.writelines( tasks_arg )
--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 prompts = {'task': 'Enter a task: ', 'action': 'Type add, show, edit, complete, or exit: ', 'grab': 'Which item would you like to edit/complete?: ', 'edit': 'Enter new task: '}
 
 import functions
+import time
     
 loop = True
 
-with open( 'task_data.txt', 'a' ) as task_file:
-    pass
+functions.init_data()
+
+now = time.strftime("%Y-%m-%d %H:%M")
+
+print( f"Welcome to the task manager! - {now}" )
+
 
 while loop:
     action = input( prompts['action'] )
@@ -13,21 +18,21 @@ while loop:
     if 'add' in action:
         new_task = action[4:]
             
-        task_list = get_tasks()
+        task_list = functions.get_tasks()
         
         task_list.append( new_task + '\n' )
         
-        write_tasks( task_list )
+        functions.write_tasks( task_list )
     
     elif 'show' in action:
-        task_list = get_tasks()
+        task_list = functions.get_tasks()
         
         for index, item in enumerate(task_list):
             print( f'{index + 1}:- {item}' )
     
     elif 'edit' in action:
         try:
-            task_list = get_tasks()
+            task_list = functions.get_tasks()
             
             grab_task = int( action[5:] ) - 1
             old_task = task_list[grab_task]
@@ -35,7 +40,7 @@ while loop:
             edit_task = input( prompts['edit'] )
             task_list[grab_task] = edit_task + '\n'
             
-            write_tasks( task_list )
+            functions.write_tasks( task_list )
         
         except ValueError:
             print( "Your command was invalid!" )
@@ -43,13 +48,13 @@ while loop:
     
     elif 'complete' in action:
         try:
-            task_list = get_tasks()
+            task_list = functions.get_tasks()
             
             grab_task = int( action[9:] ) - 1
             complete_task = task_list[grab_task]
             task_list.pop( grab_task )
             
-            write_tasks( task_list )
+            functions.write_tasks( task_list )
             
         except ValueError:
             print( "Your command was invalid!" )
