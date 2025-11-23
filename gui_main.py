680d322ea8d38fed87e04b0a7465c81a22af5652
PensiveEagle <halljf96@gmail.com>
2025-11-23 08:49:02 +0000

    Added complete feature
    Added button with functionality to complete tasks
    and remove them from the task list
    
    The interface is then updated to reflect this

--- gui_main.py
+++ gui_main.py
@@ -9,6 +9,7 @@ label = fsg.Text( 'Type in a task' )
 input_box = fsg.InputText( tooltip = 'Enter a task', key = 'task' )
 add_button = fsg.Button( 'Add' )
 edit_button = fsg.Button( 'Edit' )
+complete_button = fsg.Button( 'Complete' )
 task_list_box = fsg.Listbox( values = func.get_tasks(), 
                             key = "tasks", 
                             enable_events = True,
@@ -18,7 +19,7 @@ task_list_box = fsg.Listbox( values = func.get_tasks(),
 win_layout = [
     [label], 
     [input_box, add_button],
-    [task_list_box, edit_button]
+    [task_list_box, edit_button, complete_button]
 ]
 
 window = fsg.Window( 'Task List App', 
@@ -51,12 +52,18 @@ while loop:
             window['task'].update( value = '' ) #type: ignore
             window['tasks'].update( values = task_list ) #type: ignore
             
+        case 'Complete':
+            task_list = func.get_tasks()
+            complete_task = values['tasks']
+            complete_index = task_list.index( complete_task[0] )
+            task_list.pop( complete_index )
+            func.write_tasks( task_list )
+            window['task'].update( value = '' ) #type: ignore
+            window['tasks'].update( values = task_list ) #type: ignore         
         case 'tasks':
             window['task'].update( value = values['tasks'][0].replace( '\n', '' ) ) #type: ignore
             
         case fsg.WINDOW_CLOSED:
             break
             
-
-
 window.close()
