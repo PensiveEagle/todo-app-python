e9d84096734a908a66498072ab3a38bf780eb487
PensiveEagle <halljf96@gmail.com>
2025-11-23 08:32:37 +0000

    Added edit feature
    Added ability to edit existing tasks in the task list

--- gui_main.py
+++ gui_main.py
@@ -9,9 +9,20 @@ label = fsg.Text( 'Type in a task' )
 input_box = fsg.InputText( tooltip = 'Enter a task', key = 'task' )
 add_button = fsg.Button( 'Add' )
 edit_button = fsg.Button( 'Edit' )
+task_list_box = fsg.Listbox( values = func.get_tasks(), 
+                            key = "tasks", 
+                            enable_events = True,
+                            size = [45, 10]
+                            ) # type: ignore
+
+win_layout = [
+    [label], 
+    [input_box, add_button],
+    [task_list_box, edit_button]
+]
 
 window = fsg.Window( 'Task List App', 
-                    layout = [[label, input_box, add_button]],
+                    layout = win_layout,
                     font = ( 'Helvetica', 12 ) )
 
 
@@ -27,6 +38,24 @@ while loop:
             new_task = values['task']
             task_list.append( new_task + '\n' )
             func.write_tasks( task_list )
+            window['task'].update( value = '' ) #type: ignore
+            window['tasks'].update( values = func.get_tasks() ) #type: ignore
+            
+        case 'Edit':
+            task_list = func.get_tasks()
+            
+            edit_task = values['tasks']
+            new_task = values['task']
+            
+            print( edit_task )
+            print( new_task )
+            edit_index = task_list.index( edit_task[0] )
+            task_list[edit_index] = new_task + '\n'
+            
+            func.write_tasks( task_list )
+            
+            window['task'].update( value = '' ) #type: ignore
+            window['tasks'].update( values = task_list ) #type: ignore
             
         case fsg.WINDOW_CLOSED:
             break
