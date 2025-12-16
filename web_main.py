4f9d5e5bd0c33511ca00460ea38c808e52070378
PensiveEagle <halljf96@gmail.com>
2025-12-16 21:59:54 +0000

    Add tasks to the web app
    Added checkboxes for each task in the task_data file

--- web_main.py
+++ web_main.py
@@ -1,6 +1,11 @@
 import streamlit as st
+import functions as func
 
 st.title( "Simple To Do App" )
 st.subheader( "by PensiveEagle" )
 st.write( "This is an example app that allows a user to track tasks" )
 
+tasks = func.get_tasks( filepath = "task_data.txt" )
+
+for task in tasks:
+    st.checkbox( task )
