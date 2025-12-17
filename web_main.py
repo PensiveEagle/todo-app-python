d8438556e7eb8fd46dbdd3b51b62bb595cb5f8f0
PensiveEagle <halljf96@gmail.com>
2025-12-17 15:42:43 +0000

    Added new task addition function
    Added a function to add new tasks to the list from the input box
    So users can add new tasks

--- web_main.py
+++ web_main.py
@@ -1,7 +1,15 @@
 import streamlit as st
 import functions as func
 
-tasks = func.get_tasks( filepath = "task_data.txt" )
+func.init_data()
+
+tasks = func.get_tasks()
+
+def add_task():
+    task = st.session_state["new_task"] + "\n"
+    tasks.append( task )
+    func.write_tasks( tasks )
+
 
 st.title( "Simple To Do App" )
 st.subheader( "by PensiveEagle" )
@@ -10,4 +18,8 @@ st.write( "This is an example app that allows a user to track tasks" )
 for task in tasks:
     st.checkbox( task )
     
-st.text_input( label = "", placeholder = "Enter a new task..." )
+st.text_input( label = "Enter a new task", 
+              placeholder = "Enter a new task...",
+              label_visibility = "hidden",
+              on_change = add_task,
+              key = "new_task" )
